2f53ec432a20449cf057a829122ebe205b4af4ed
Andrew Davis <andrewdavis@wattopoststudio.com>
2016-07-27 15:13:49 -0400

    Hello World - First Component
    https://egghead.io/lessons/react-hello-world-first-component

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react'
-class App extends React.Component{
-  render(){
-    return <div>hI</div>
-  }
-}
+
+/* This is stateful */
+// class App extends React.Component{
+//   render(){
+//     // return React.createElement('h1', null, 'Hello Guys') //Alternative way
+//     return <h1>Hello guys!</h1>
+//   }
+// }
+
+/* This is stateless */
+const App = () => <h1>Hello Eggheads</h1>
 
 export default App
