610fc749f0cd14d375f8ccc9ce9e07c690216109
Andrew Davis <andrewdavis@wattopoststudio.com>
2016-07-28 17:47:12 -0400

    Composable Components https://egghead.io/lessons/react-composable-components

--- main.js
+++ main.js
@@ -2,34 +2,75 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component{
+
   constructor(){
     super()
+    this.state = {
+      red: 0
+    }
     this.update = this.update.bind(this)
-    this.state = { increasing: false }
   }
 
-  update(){
-    ReactDOM.render(<App val={this.props.val+1} />, document.getElementById('app'))
-  }
+  update(e){
 
-  componentWillReceiveProps(nextProps){
-    this.setState({increasing: nextProps.val > this.props.val})
-  }
-
-  shouldComponentUpdate(nextProps, nexState){
-    return nextProps.val % 5 === 0
-  }
-
-  componentDidUpdate(prevProps, prevState){
-    console.log('prevProps', prevProps)
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.inp.refs.red).value
+    })
   }
 
   render(){
-    console.log(this.state.increasing)
-    return (<button onClick={this.update}>{this.props.val}</button>)
+  
+    return (
+      <div>
+        <NumInput
+          ref="inp"
+          min={0}
+          max={255}
+          step={1}
+          val={+this.state.red}
+          label="Red"
+          update={this.update} />
+      </div>
+    )
   }
 }
 
-App.defaultProps = { val: 0 }
+class NumInput extends React.Component {
+  render(){
+    return (
+      <div>
+        <label>Val: {this.props.val}</label>
+        <input
+          ref="red"
+          type={this.props.type}
+          min={this.props.min}
+          max={this.props.max}
+          step={this.props.step}
+          defaultValue={this.props.val}
+          onChange={this.props.update}
+        />
+      </div>
+    )
+  }
+}
+
+NumInput.propTypes = {
+  min: React.PropTypes.number,
+  max: React.PropTypes.number,
+  step: React.PropTypes.number,
+  val: React.PropTypes.number,
+  label: React.PropTypes.string,
+  update: React.PropTypes.func.isRequired,
+  type: React.PropTypes.oneOf(['number', 'range'])
+}
+
+NumInput.defaultProps = {
+  min: 0,
+  max: 0,
+  step: 1,
+  val: 0,
+  label: '',
+  type: 'range'
+}
 
 ReactDOM.render(<App />, document.getElementById('app'))
