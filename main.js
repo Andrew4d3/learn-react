50fad5d728495010e6b304bef6331939368d3ec7
Andrew Davis <andrewdavis@wattopoststudio.com>
2016-07-28 23:04:17 -0400

    Higher Order Components (replaces Mixins) https://egghead.io/lessons/react-react-fundamentals-higher-order-components-replaces-mixins

--- main.js
+++ main.js
@@ -1,76 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component{
-
+let Mixin = InnerComponent => class extends React.Component {
   constructor(){
     super()
-    this.state = {
-      red: 0
-    }
+    this.state = { val: 0}
     this.update = this.update.bind(this)
   }
 
-  update(e){
+  update(){
+    this.setState({val: this.state.val + 1})
+  }
 
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.inp.refs.red).value
-    })
+  componentWillMount(){
+    console.log('will unmount');
+  }
+
+  componentDidMount(){
+    console.log('mounted');
   }
 
   render(){
-  
-    return (
-      <div>
-        <NumInput
-          ref="inp"
-          min={0}
-          max={255}
-          step={1}
-          val={+this.state.red}
-          label="Red"
-          update={this.update} />
-      </div>
-    )
+    return (<InnerComponent update={this.update} {...this.state} {...this.props} />)
   }
+
 }
 
-class NumInput extends React.Component {
+const Button = (props) =>{
+  return (
+    <button onClick={props.update}>
+      {props.txt} - {props.val}
+    </button>
+  )
+}
+
+const Label = (props) =>{
+  return (
+    <label onMouseMove={props.update}>
+      {props.txt} - {props.val}
+    </label>
+  )
+}
+
+let ButtonMixed = Mixin(Button)
+let LabelMixed = Mixin(Label)
+
+class App extends React.Component{
   render(){
-    return (
-      <div>
-        <label>Val: {this.props.val}</label>
-        <input
-          ref="red"
-          type={this.props.type}
-          min={this.props.min}
-          max={this.props.max}
-          step={this.props.step}
-          defaultValue={this.props.val}
-          onChange={this.props.update}
-        />
-      </div>
-    )
+    return (<div><ButtonMixed txt="Button" /><LabelMixed txt="Button" /></div>)
   }
 }
 
-NumInput.propTypes = {
-  min: React.PropTypes.number,
-  max: React.PropTypes.number,
-  step: React.PropTypes.number,
-  val: React.PropTypes.number,
-  label: React.PropTypes.string,
-  update: React.PropTypes.func.isRequired,
-  type: React.PropTypes.oneOf(['number', 'range'])
-}
-
-NumInput.defaultProps = {
-  min: 0,
-  max: 0,
-  step: 1,
-  val: 0,
-  label: '',
-  type: 'range'
-}
 
 ReactDOM.render(<App />, document.getElementById('app'))
